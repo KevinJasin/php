896d4b377b9fa2f30013ca1163e9dd01df430ef5
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-10-19 14:20:29 +0300

    Update and rename php1 to index.php

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once("connect.php");
+
+
+
+$stmt = $pdo->query('SELECT * FROM books');
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+</head>
+<body>
+
+<?php
+while ($row = $stmt->fetch()) {
+    ?> 
+    <li>
+        <a href="book.php?id=<?= $row["id"]; ?>">
+        <?=$row['title']; ?>
+        
+    </a>
+    
+    </li>;
+
+<?php    
+}
+?>
+
+</body>
+</html>
+
+<?php
