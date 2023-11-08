a24da159d0c9b8b83cf06408b1156d279684c8c8
KevinJasin <112690988+KevinJasin@users.noreply.github.com>
2023-11-08 13:41:56 +0200

    Update index.php

--- index.php
+++ index.php
@@ -16,36 +16,43 @@ $stmt = $pdo->query('SELECT * FROM books');
 </head>
 <body>
 
+<!-- (A) SEARCH FORM -->
+<form method="post" action="2-form.php">
+  <input type="text" name="search" placeholder="Search..." required>
+  <input type="submit" value="Search">
+</form>
+
+<?php
+// (B) PROCESS SEARCH WHEN FORM SUBMITTED
+if (isset($_POST["search"])) {
+  // (B1) SEARCH FOR USERS
+  require "3-search.php";
+
+  // (B2) DISPLAY RESULTS
+  if (count($results) > 0) { foreach ($results as $r) {
+    printf("<div>%s - %s</div>", $r["name"], $r["email"]);
+  }} else { echo "No results found"; }
+}
+?>
+
+
 <?php
 while ($row = $stmt->fetch()) {
     ?> 
     <li>
         <a href="book.php?id=<?= $row["id"]; ?>">
         <?=$row['title']; ?>
-        
+
     </a>
-    
+
     </li>;
 
 
 
     <?php
-while ($row = $stmt->fetch()) {
-    ?> 
-    <select name="" id="">
-    <li>
-        <a href="book.php?id=<?= $row["id"]; ?>">
-        <?=$row['first_name']; ?> <?= $row["last_name"]; ?>
-        
-    </a>
-    
-    </li>;
-    </select>
-<?php    
 }
 ?>
 
 </body>
 </html>
 
-<?php
